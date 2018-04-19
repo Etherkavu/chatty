7455feb3a2071c4f67e867a23b2017c5effb65f9
Brian Holden <bkavuh@gmail.com>
2018-04-19 02:25:41 +0000

    MEssages now updating

--- src/App.jsx
+++ src/App.jsx
@@ -12,33 +12,45 @@ class App extends Component {
     super(props);
 
     this.state = {
-
-  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-  messages: [
-    {
-      id : "1",
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-    },
-    {
-      id: "2",
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-    },
-    {
-      id : "3",
-      username: "JimBob",
-      content: "I found them!",
-    },
-  ]
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          id : "1",
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          id: "2",
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        },
+      ]
     };
-
+    this.onSubmit = this.onSubmit.bind(this);
   }
+
+    componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+
+      const newMessage = {id: this.state.messages.length+1, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      this.setState({messages: messages})
+    }, 3000);
+  }
+
+  onSubmit(newSubmit) {
+      const newMessage = {id: this.state.messages.length+1, username: this.state.currentUser.name, content: newSubmit};
+    const messages = this.state.messages.concat(newMessage)
+    this.setState({messages: messages})
+  }
+
   render() {
     return (
       <div>
         <MessageList messages={ this.state.messages } />
-        <Footer currentUser={ this.state.currentUser } />
+        <Footer currentUser={ this.state.currentUser } onSubmit={ this.onSubmit } />
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 
-class ChatBox extends Component {
+
+
+class ChatBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: '',
+    }
+    this.onEnter = this.onEnter.bind(this);
+  }
+
+
+  onEnter(event){
+    if (event.key === 'Enter') {
+      this.props.onSubmit(event.target.value)
+    }
+  }
+
+
+
   render() {
     return (
       <footer className="chatbar">
         <input className="chatbar-username" placeholder={ this.props.currentUser.name } />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input
+          className="chatbar-message"
+          placeholder="Type a message and hit ENTER"
+          onKeyPress={this.onEnter}
+        />
       </footer>
     )
   }
 }
 
-export default ChatBox;
+
+export default ChatBar;
